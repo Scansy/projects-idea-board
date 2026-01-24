e34b052b5545be8da3adf61596460ce9e7a77c3a
Ryan He <rha136@sfu.ca>
2026-01-23 22:24:01 -0800

    it can fetch data from JSONplaceholder but it can't fetch data from http://localhost:6767/sixseven, I'm being cucked

--- src/frontend/main.ts
+++ src/frontend/main.ts
@@ -1,4 +1,29 @@
 
 function testFunction(): void{
-    alert("button works");
+    alert('working');
+    const request = new XMLHttpRequest();
+    
+    request.addEventListener('readystatechange', () =>{
+        //console.log(request, request.readyState);
+        if(request.readyState === 4 && request.status === 200){
+            callback(undefined, request.responseText)
+        }else if(request.readyState === 4){
+            //request is finished but there's some sort of problem
+            callback('could not fetch data', undefined);
+        }
+    });
+    
+    request.open('GET', 'http://localhost:6767/sixseven');
+    request.send();
 }
+
+function callback(err:any, data:any){
+    console.log('callback fired')
+    if(err){
+        console.log(err);
+    }else{
+        console.log(data);
+    }
+}
+
+
