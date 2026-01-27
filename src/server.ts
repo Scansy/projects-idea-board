176aafa1e6c493c3718023031780777baa10a2d6
Liuenne <eltonchen2004@gmail.com>
2026-01-27 13:16:09 -0800

    fucking kill ryan

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,11 @@
 import express, { type Request, type Response } from 'express';
+import cors from 'cors';
 
 const app = express();
 const PORT: number = 6767;
+
+app.use(cors());
+app.use(express.json());
 
 //le listen
 app.get('/', (req: Request, res: Response) => {
