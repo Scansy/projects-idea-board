30c0400c9ed1ffb672c59f579dacac593fe7de7f
Liuenne <eltonchen2004@gmail.com>
2026-02-01 01:48:57 -0800

    server moved to separate directory, typescript not js

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App(): JSX.Element {
+function App() {
   const testFunction = (): void => {
     alert('working');
-    const request = new XMLHttpRequest();
 
+    const request = new XMLHttpRequest();
     request.addEventListener('readystatechange', () => {
       if (request.readyState === 4 && request.status === 200) {
-        console.log('callback fired');
         console.log(request.responseText);
-      } else if (request.readyState === 4) {
-        console.log('callback fired');
-        console.log('could not fetch data');
       }
     });
 
@@ -22,24 +18,7 @@ function App(): JSX.Element {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-
-        <button onClick={testFunction}>Click me</button>
-
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={testFunction}>Click me</button>
     </div>
   );
 }
